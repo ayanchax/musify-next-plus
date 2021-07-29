7b37ea7188fddb6203457561d8bf4d1175db6f82
Swaroop <swaroop.chakraborty@cerner.com>
2021-07-29 03:38:43 -0400

    comit vercel

--- configurations/endpoint.js
+++ configurations/endpoint.js
@@ -2,8 +2,8 @@ import { ENV } from "../configurations/environments";
 export const api_key = "5ET4NH9-4M64SNC-J3X5MGD-7MHPYZB";
 export const ASYNC_CALLER_URL = () => {
     return ENV === "prod" ?
-        "https://ec2-13-126-109-226.ap-south-1.compute.amazonaws.com/api/" +
+        "http://ec2-13-233-236-190.ap-south-1.compute.amazonaws.com:3000/api/" +
         api_key +
         "/" :
-        "http://localhost:7000/api/" + api_key + "/";
+        "http://localhost:8000/api/" + api_key + "/";
 };
