1265f082cd8bb9de05fa9cc90ef68d6b4d6eef63
Swaroop <swaroop.chakraborty@cerner.com>
2021-08-01 14:23:18 -0400

    commit

--- configurations/environments.js
+++ configurations/environments.js
@@ -6,9 +6,9 @@ export const MAINT_TITLE = "Musify | Maintenance Mode";
 export const SITE_NAME = "Musify";
 export const SITE_TITLE = "Musify | Enjoy Ad Free Premium Content Music";
 export const SITE_DESC =
-    "Tired of listening to ads and premium subscriptions while streaming music? Well you might like my new music streaming app #musify in this case. Pls visit " +
+    "Tired of listening to ads and premium subscriptions while streaming music? Well you might like the new music streaming app #musify. Pls visit " +
     BASE +
-    " for fresh music content ad free. You just need to sign in using your google credentials and enjoy high quality(320, 640, 1080kbps) ad free music.";
+    " for fresh music content ad free. You just need to sign in with your google credentials and enjoy high quality(320, 640, 1080kbps) ad free music.";
 export const SITE_IMAGE = "https://musify-plus.vercel.app/musify-share-poster.png";
 export const SITE_HOME_URL = BASE;
 export const SITE_HASH = "#musify";
